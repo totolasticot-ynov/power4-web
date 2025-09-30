008f06b1e835be9ba31b75e104d8f850573d9f8f
dhthomas <thomas.dhordain@ynov.com>
2025-09-30 15:35:29 +0200

    toto

--- main/main.go
+++ main/main.go
@@ -12,4 +12,18 @@ func main() {
 
 	fmt.Println("Serveur démarré sur le port 8080...")
 	http.ListenAndServe(":8080", nil)
+
+	// Dossier qui contient tes fichiers HTML, CSS, JS, images...
+	fs := http.FileServer(http.Dir("./static"))
+
+	// Tout ce qui commence par /static ira chercher dans ./static
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	// Page d’accueil
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./static/index.html")
+	})
+
+	// Lancer le serveur
+	http.ListenAndServe(":8080", nil)
 }
