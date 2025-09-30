195c660c32fd7cfbc3ea161a6c7ace5a79d3eb6e
dhthomas <thomas.dhordain@ynov.com>
2025-09-30 17:55:41 +0200

    pour etre sur que c bon

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"log"
+
+	"github.com/totolasticot-ynov/power4-web/src/menu"
+)
+
 func main() {
-	menu()
+	// On lance le menu, et on gère une éventuelle erreur
+	if err := menu.Menu(); err != nil {
+		log.Fatalf("Erreur lors de l’exécution du menu : %v", err)
+	}
 }
