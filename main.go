3a869a3aac3c470a4294ffdae45ea71e2fcdc66c
ThomasBeyney <thomas.beyney@ynov.com>
2025-10-01 14:20:10 +0200

    main

--- main.go
+++ main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"log"
-	"power4-web/src/menu"
+    "log"
+    "power4-web/src/menu"
 )
 
 func main() {
-	// On lance le menu, et on gère une éventuelle erreur
-	if err := menu.Menu(); err != nil {
-		log.Fatalf("Erreur lors de l’exécution du menu : %v", err)
-	}
+    // On lance le menu, et on gère une éventuelle erreur
+    if err := menu.Menu(); err != nil {
+        log.Fatalf("Erreur lors de l’exécution du menu : %v", err)
+    }
 }
