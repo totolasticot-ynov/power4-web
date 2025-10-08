c923949802e2f3b9d79b2f3ea0aa5bedee36e6c4
dhthomas <thomas.dhordain@ynov.com>
2025-10-08 14:03:15 +0200

    reglage de bug

--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-    "log"
-    "github.com/totolasticot-ynov/power4-web/src/menu"
+	"log"
+
+	"github.com/totolasticot-ynov/power4-web/src/menu"
 )
 
 func main() {
-    // On lance le menu, et on gère une éventuelle erreur
-    if err := menu.Menu(); err != nil {
-        log.Fatalf("Erreur lors de l’exécution du menu : %v", err)
-    }
+	// On lance le menu, et on gère une éventuelle erreur
+	if err := menu.Menu(); err != nil {
+		log.Fatalf("Erreur lors de l’exécution du menu : %v", err)
+	}
 }
