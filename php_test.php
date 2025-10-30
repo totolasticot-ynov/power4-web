33686e8f4a1ca90f64182ef07a27ea4adbb92a70
ThomasBeyney <thomas.beyney@ynov.com>
2025-10-30 15:13:07 +0100

    Base de donnée

--- php_test.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Simple PHP test file to verify Apache+PHP serve this project folder
-echo "PHP OK";
-
-// Uncomment the following line if you want full phpinfo (for debugging only)
-// phpinfo();
-
-?>
